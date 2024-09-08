41dad3a83605edf9568a7b68f0b06e3875fa764f
shahriar.rubayet <shahriar.rubayet@upaybd.com>
2024-09-08 13:34:54 +0600

    add: pydanctic things

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating : Optional[int] = None
 
 #decorator
 @app.get("/")
@@ -20,5 +23,8 @@ def get_posts():
 @app.post("/createposts")
 def create_posts(new_post: Post):
     print(new_post.title)
-    return {"data": "new post"}
+    print(new_post.content)
+    print(new_post.published)
+    print(new_post.rating)
+    return {"data": "new post"} 
 
